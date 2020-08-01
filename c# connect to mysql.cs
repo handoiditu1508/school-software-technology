ca844bce0edd0ae9f06d7afc133f9df80ddccab9
handoiditu1508 <handoiditu1508@gmail.com>
2020-08-01 20:05:05 +0700

    19:56 2020-08-01

---
+++ c# connect to mysql.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=food raider;SslMode=none";
+			string query = "SELECT * FROM clients";
+
+			MySqlConnection connection = new MySqlConnection(connectionString);
+			MySqlCommand command = new MySqlCommand(query, connection);
+			MySqlDataReader reader;
+			try
+			{
+				connection.Open();
+				reader = command.ExecuteReader();
+				while(reader.Read())
+				{
+					Console.WriteLine(reader["ID"]);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				connection.Close();
+			}
+			Console.ReadKey();
+		}
+	}
+}
